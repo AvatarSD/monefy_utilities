039944c7fdfd8a4458fea11d9e92f45e42a37e45
avatarsd <avatarsd2@gmail.com>
2023-05-24 02:20:05 +0300

    Print xls file passed by argv[1]

---
+++ moneyfee_inspect_xlsx.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+
+if len(sys.argv) < 2: 
+    sys.exit()
+
+moneyfee_table = pd.read_excel(sys.argv[1], engine='openpyxl')
+
+print("Shape of import", moneyfee_table.shape)
+print(moneyfee_table)
