27e9f6575926729e0ef6fcf9a2fdad31cd558f84
avatarsd <avatarsd2@gmail.com>
2023-05-27 07:02:00 +0300

    Compleate saving 'mm_table' into '*.xlsx'

--- moneyfee_inspect_xlsx.py
+++ moneyfee_inspect_xlsx.py
@@ -30,12 +30,27 @@ class money_move:
         self.categoty_sub = str("")        # Підкатегорія
         self.agent = str("")               # Контрагент
         self.project = str("")             # Проект
-        self.tags = []                     # Теги
+        self.tags = str("")                # Теги
         self.comment = str("")             # Коментар		
 
     def to_str(self) -> str:
         return str("dat: ") + str(self.date) + str("\t\tamo: ") + str(self.amount) + str("\t\tamo_com: ") + str(self.amount_company) + str("\t\tatx: ") + str(self.account_tx) + str("\t\tarx: ") + str(self.account_rx) + str("\t\tcat: ") + str(self.categoty) + str("\t\tmsg: ") + str(self.comment) + str("\t\t")
 
+    def row(self):
+        return [self.date, 
+                self.date_income, 
+                self.date_begin, 
+                self.date_end, 
+                self.amount, 
+                self.amount_company, 
+                self.account_tx, 
+                self.account_rx, 
+                self.categoty, 
+                self.categoty_sub, 
+                self.agent, 
+                self.project, 
+                self.tags, 
+                self.comment ]
 
 # PARSE ARGS
 if len(sys.argv) < 2: 
@@ -48,7 +63,20 @@ moneyfee_table = pd.read_excel(sys.argv[1], engine='openpyxl')
 moneyfee_table.columns = ['date', 'account', 'category', 'amount', 'currency.1', "converted amount", "converted currency", "description"]
 
 def_currency = str("UAH")
-mm_table = []
+mm_table = pd.DataFrame(columns=['date',
+                                 'date_income',
+                                 'date_begin',
+                                 'date_end',
+                                 'amount',
+                                 'amount_company',
+                                 'account_tx',
+                                 'account_rx',
+                                 'categoty',
+                                 'categoty_sub',
+                                 'agent',
+                                 'project',
+                                 'tags',
+                                 'comment'])
 
 print(moneyfee_table)
 print("Shape of import", moneyfee_table.shape)
@@ -118,7 +146,7 @@ for i,row in moneyfee_table.iterrows():
             mm.account_tx = old_acc if dir == dir_t.INVERT else new_acc
             mm.categoty = str("transfer") if currency == def_currency else str("exchange")
             mm.comment = old_acc + str(" ") + new_acc
-            mm_table.append(mm)
+            mm_table.loc[len(mm_table)] = mm.row()
 
             # 1.2.5 break, continue
             break
@@ -138,7 +166,7 @@ for i,row in moneyfee_table.iterrows():
         mm.categoty = category
         dsc = row["description"]
         mm.comment = str("") if ((dsc is None) or (str("nan") == str(dsc))) else row["description"]
-        mm_table.append(mm)
+        mm_table.loc[len(mm_table)] = mm.row()
 
         # 1.3.2 save stats transaction
         if dir == dir_t.INVERT:
@@ -153,10 +181,11 @@ print('>>>>\r\n')
 # 3. Check unhandled; Print inspection stat
 ungandled = moneyfee_table.loc[(moneyfee_table['handled'] == False)]
 print(ungandled)
-# ungandled.to_excel("unhandled.xlsl")
+# if len(ungandled): ungandled.to_excel("unhandled.xlsl")
 print(f'Solds: {stats.sell}, stats.buys: {stats.buy}, stats.transaction: {stats.transaction}, conv: {stats.conversion}, Err: {len(ungandled)}')
 print(f'>> Total: {stats.sell+stats.buy+stats.transaction*2+stats.conversion*2+len(ungandled)}/{len(moneyfee_table)}')
 
 # 4. Save new list in specific format
-for mm in mm_table:
-    print(mm.to_str())
+mm_table.to_excel("unhandled.xlsx")
+# for mm in mm_table:
+#     print(mm.to_str())
